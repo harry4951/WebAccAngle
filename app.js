c839e795d41048f734509596bbcf45b1bb79993f
Chengjie <harry4951@gmail.com>
2019-09-05 16:12:32 +0200

    Play sound 5

--- app.js
+++ app.js
@@ -52,85 +52,84 @@ function handleHeartRateMeasurement(heartRateMeasurement) {
     heartRates.push(heartRateMeasurement.heartRate);
     //console.log(heartRates);
     //drawWaves();
-    //drawWaves();
   });
 }
 
-// function playSound () {
-//   document.getElementById('play').play();
+ function playSound () {
+   document.getElementById('play').play();
+ }
+
+// var sounds = {
+//   "dead" : {
+//     url : "sounds/dead.mp3"
+//   },
+//   "smash" : {
+//     url : "sounds/smash.mp3",
+//   },
+//   "ping" : {
+//     url : "sounds/ping.mp3"
+//   },
+//   "bump" : {
+//     url : "sounds/bump.mp3"
+//   },
+//   "jump" : {
+//     url : "sounds/jump.wav"
+//   },
+//   "coin" : {
+//     url : "sounds/coin.mp3"
+//   }
+// };
+
+
+// var soundContext = new AudioContext();
+
+// for(var key in sounds) {
+//   loadSound(key);
 // }
 
-var sounds = {
-  "dead" : {
-    url : "sounds/dead.mp3"
-  },
-  "smash" : {
-    url : "sounds/smash.mp3",
-  },
-  "ping" : {
-    url : "sounds/ping.mp3"
-  },
-  "bump" : {
-    url : "sounds/bump.mp3"
-  },
-  "jump" : {
-    url : "sounds/jump.wav"
-  },
-  "coin" : {
-    url : "sounds/coin.mp3"
-  }
-};
+// function loadSound(name){
+//   var sound = sounds[name];
 
+//   var url = sound.url;
+//   var buffer = sound.buffer;
 
-var soundContext = new AudioContext();
+//   var request = new XMLHttpRequest();
+//   request.open('GET', url, true);
+//   request.responseType = 'arraybuffer';
 
-for(var key in sounds) {
-  loadSound(key);
-}
+//   request.onload = function() {
+//     soundContext.decodeAudioData(request.response, function(newBuffer) {
+//       sound.buffer = newBuffer;
+//     });
+//   }
 
-function loadSound(name){
-  var sound = sounds[name];
+//   request.send();
+// }
 
-  var url = sound.url;
-  var buffer = sound.buffer;
+// function playSound(name, options){
+//   var sound = sounds[name];
+//   var soundVolume = sounds[name].volume || 1;
 
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.responseType = 'arraybuffer';
+//   var buffer = sound.buffer;
+//   if(buffer){
+//     var source = soundContext.createBufferSource();
+//     source.buffer = buffer;
 
-  request.onload = function() {
-    soundContext.decodeAudioData(request.response, function(newBuffer) {
-      sound.buffer = newBuffer;
-    });
-  }
+//     var volume = soundContext.createGain();
 
-  request.send();
-}
+//     if(options) {
+//       if(options.volume) {
+//         volume.gain.value = soundVolume * options.volume;
+//       }
+//     } else {
+//       volume.gain.value = soundVolume;
+//     }
 
-function playSound(name, options){
-  var sound = sounds[name];
-  var soundVolume = sounds[name].volume || 1;
-
-  var buffer = sound.buffer;
-  if(buffer){
-    var source = soundContext.createBufferSource();
-    source.buffer = buffer;
-
-    var volume = soundContext.createGain();
-
-    if(options) {
-      if(options.volume) {
-        volume.gain.value = soundVolume * options.volume;
-      }
-    } else {
-      volume.gain.value = soundVolume;
-    }
-
-    volume.connect(soundContext.destination);
-    source.connect(volume);
-    source.start(0);
-  }
-}
+//     volume.connect(soundContext.destination);
+//     source.connect(volume);
+//     source.start(0);
+//   }
+// }
 
 
 var heartRates = [];
